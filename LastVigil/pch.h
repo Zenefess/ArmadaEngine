5270b71f8a2aca8c5b29f96d2e415fce5aa4ac1c
David William Bull <116688139+Zenefess@users.noreply.github.com>
2024-03-07 20:22:22 +1100

    Add files via upload

---
+++ LastVigil/pch.h
@@ -0,0 +1,28 @@
+/************************************************************
+ * File: pch.h                          Created: 2022/10/09 *
+ *                                Last modified: 2022/10/09 *
+ *                                                          *
+ * Desc:                                                    *
+ *                                                          *
+ *  Copyright (c) David William Bull. All rights reserved.  *
+ ************************************************************/
+#pragma once
+#pragma warning(once:4244)
+#pragma warning(once:4267)
+#pragma warning(once:4838)
+
+
+#ifndef PCH_H
+	#define PCH_H
+
+	#include "targetver.h"
+
+	#define WIN32_LEAN_AND_MEAN
+
+	#include <windows.h>
+	#include <process.h>
+	#include <stdlib.h>
+	#include <malloc.h>
+	#include <memory.h>
+	#include <tchar.h>
+#endif
