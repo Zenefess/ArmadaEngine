12665aede9f2d9b0b486cbf6714de3db01ddb339
David William Bull <116688139+Zenefess@users.noreply.github.com>
2024-03-07 20:15:17 +1100

    Add files via upload

---
+++ LastVigil.h
@@ -0,0 +1,11 @@
+/************************************************************
+ * File: LastVigil.h                    Created: 2020/10/09 *
+ *                                Last modified: 2020/10/29 *
+ *                                                          *
+ * Desc: Defines                                            *
+ *                                                          *
+ *  Copyright (c) David William Bull. All rights reserved.  *
+ ************************************************************/
+#pragma once
+
+#include "resource.h"
