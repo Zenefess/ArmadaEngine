0aa00769c3265364d5fcdc0d7e7af290cd352a35
David William Bull <116688139+Zenefess@users.noreply.github.com>
2024-03-07 20:35:58 +1100

    Add files via upload

---
+++ LastVigil/Include/WorldGen threads.h
@@ -0,0 +1,23 @@
+/**********************************************************
+ * File: WorldGen threads.h             Created: 28/10/08 *
+ *                                Last modified: 28/10/08 *
+ *                                                        *
+ * Desc:                                                  *
+ *                                                        *
+ * Copyright (c) David William Bull. All rights reserved. *
+ **********************************************************/
+
+#include "typedefs.h"
+#include "Data structures.h"
+#include "Command queue.h"
+//#include "Armada Intelligence\class_entitymanager.h"
+
+ // Thread status strings
+extern cwchptr stInit;
+extern cwchptr stBusy;
+extern cwchptr stError;
+cwchptr stWGen = L"TankEngine";
+
+extern al16 vui64  THREAD_LIFE;			// 'Thread active' flags
+extern al16 wchptr stThrdStat;			// Text strings for thread status
+extern al8  HWND   hWnd;					// Main window's handle
